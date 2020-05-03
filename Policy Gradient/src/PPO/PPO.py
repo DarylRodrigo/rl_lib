0b417ad0afcde7947d7a288371427f750d2fb6c5
Daryl Rodrigo <darylrodrigo@gmail.com>
2020-05-03 09:57:58 +0100

    first PPO version works

---
+++ Policy Gradient/src/PPO/PPO.py
@@ -0,0 +1,75 @@
+from ActorCritic import ActorCritic
+from Memory import Memory
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+import copy
+import pdb
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class PPO:
+  def __init__(self, state_space, action_space, hidden_size=64, epsilon=0.2, entropy_beta=0.01, gamma=0.99):
+    self.mem = Memory()
+
+    self.gamma = gamma
+    self.epsilon = epsilon
+    self.entropy_beta = entropy_beta
+
+    self.model = ActorCritic(state_space, action_space, hidden_size)
+    self.model_old = ActorCritic(state_space, action_space, hidden_size) 
+
+    self.model_old.load_state_dict(self.model.state_dict())
+
+    self.optimiser = optim.Adam(self.model.parameters(), lr=1e-3)
+  
+  def act(self, x):
+    return self.model_old.act(x)
+
+  def learn(self, num_learn):
+    # Calculate discounted rewards
+    discounted_returns = []
+    running_reward = 0
+
+    for reward, done in zip(reversed(self.mem.rewards), reversed(self.mem.dones)):
+      if done:
+        running_reward = 0
+      running_reward = reward + self.gamma * running_reward
+
+      discounted_returns.insert(0,running_reward)
+
+    # normalise rewards
+    discounted_returns = torch.FloatTensor(discounted_returns)
+    discounted_returns = (discounted_returns - discounted_returns.mean()) / (discounted_returns.std() + 1e-5)
+
+    prev_states = torch.stack(self.mem.states).to(device).detach()
+    prev_actions = torch.stack(self.mem.actions).to(device).detach()
+    prev_log_probs = torch.stack(self.mem.log_probs).to(device).detach()
+
+    for i in range(num_learn):
+
+      # find ratios
+      actions, log_probs, values, entropy = self.model.evaluate(prev_states, prev_actions)
+      ratio = torch.exp(log_probs - prev_log_probs.detach())
+
+      actions, log_probs, values, entropy = self.model.evaluate(prev_states, prev_actions)
+      actions, log_probs, values, entropy = self.model_old.evaluate(prev_states, prev_actions)
+
+      # calculate advantage
+      advantage = discounted_returns - values.detach()
+
+      # calculate surrogates
+      surrogate_1 = ratio * advantage
+      surrogate_2 = torch.clamp(advantage, 1-self.epsilon, 1+self.epsilon)
+      loss = -torch.min(surrogate_1, surrogate_2) + F.mse_loss(values, discounted_returns) - self.entropy_beta*entropy
+      
+      # pdb.set_trace()
+
+      loss = loss.mean()
+
+      # calculate gradient
+      self.optimiser.zero_grad()
+      loss.backward()
+      self.optimiser.step()
+    
+    self.model_old.load_state_dict(self.model.state_dict())
